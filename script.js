585d69223550de52226acdff55ba3aea56fe05fa
Siyong <huangsy222@gmail.com>
2019-12-24 20:04:15 -0800

    more stuff

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+var headerBar = document.getElementById("headerBar");
+var headerYOffset = headerBar.offsetTop;
+function stickyHeaderCheck()
+{
+	if(window.pageYOffset > headerYOffset)
+	{
+		headerBar.classList.remove("headerNoStick");
+		headerBar.classList.add("headerStick");
+	}
+	else
+	{
+		headerBar.classList.remove("headerStick");
+		headerBar.classList.add("headerNoStick");
+	}
+}
 
+
+
+window.onscroll = function()
+{
+	stickyHeaderCheck();
+};
