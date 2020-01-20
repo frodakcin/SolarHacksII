b0c43187bd779ae706ab901466b2d588b04d7911
Siyong <huangsy222@gmail.com>
2020-01-20 00:04:23 -0800

    lots of changes

--- script.js
+++ script.js
@@ -20,3 +20,18 @@ window.onscroll = function()
 {
 	stickyHeaderCheck();
 };
+
+function FAQDrop(item)
+{
+	para = item.children[1];
+	if(para.classList.contains("FAQBoxOnP"))
+	{
+		item.classList.remove("FAQBoxOn");
+		para.classList.remove("FAQBoxOnP");
+	}
+	else
+	{
+		item.classList.add("FAQBoxOn");
+		para.classList.add("FAQBoxOnP");
+	}
+}	
