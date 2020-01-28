1c44ffec919ab9fe67cc26c32c499203c66242a3
Siyong <huangsy222@gmail.com>
2020-01-27 21:58:53 -0800

    Team Info
    Reworked header system (remove javascript & vertically centered text while fixing semantic errors).
    
    Also added email/linkedin for some team members

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-var headerBar = document.getElementById("headerBar");
-var headerYOffset = headerBar.offsetTop;
-function stickyHeaderCheck()
-{
-	if(window.pageYOffset > headerYOffset)
-	{
-		headerBar.classList.remove("headerNoStick");
-		headerBar.classList.add("headerStick");
-	}
-	else
-	{
-		headerBar.classList.remove("headerStick");
-		headerBar.classList.add("headerNoStick");
-	}
-}
-window.onscroll = stickyHeaderCheck;
-
 
 function FAQDrop(item)
 {
